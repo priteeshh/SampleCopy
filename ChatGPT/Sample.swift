2e48f17cd45ab0b6e85a24ff9ac6955b2b5f0451
priteeshh@gmail.com <priteeshh@gmail.com>
2023-01-09 02:17:13 +0530

    first commit

---
+++ ChatGPT/Sample.swift
@@ -0,0 +1,20 @@
+//
+//  Sample.swift
+//  ChatGPT
+//
+//  Created by Preeteesh Remalli on 08/01/23.
+//
+
+import SwiftUI
+
+struct Sample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sample_Previews: PreviewProvider {
+    static var previews: some View {
+        Sample()
+    }
+}
